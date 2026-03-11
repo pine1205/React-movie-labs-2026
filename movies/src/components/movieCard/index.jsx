cff936e3e0968c2ae463af55afba0e0c0d5f281f
Yuliia <20105855@setu.ie>
2026-03-11 11:03:21 +0000

    Card icons - make action icons on movie card configurable.

--- movies/src/components/movieCard/index.jsx
+++ movies/src/components/movieCard/index.jsx
@@ -20,7 +20,8 @@ import { MoviesContext } from "../../contexts/moviesContext";
 
 
 
-export default function MovieCard({ movie }) { 
+export default function MovieCard({ movie, action }) {
+
 
   const { favorites, addToFavorites } = useContext(MoviesContext);
 
@@ -78,20 +79,18 @@ export default function MovieCard({ movie }) {
           </Grid>
         </Grid>
       </CardContent>
-      <CardActions disableSpacing>
-              <IconButton aria-label="add to favorites" onClick={handleAddToFavorite}>
-        <FavoriteIcon color="primary" fontSize="large" />
-    </IconButton>
-
-
-                  <Link to={`/movies/${movie.id}`}>
+            <CardActions disableSpacing>
+      
+        {action(movie)}
+      
+        <Link to={`/movies/${movie.id}`}>
           <Button variant="outlined" size="medium" color="primary">
             More Info ...
           </Button>
         </Link>
-
-
+        
       </CardActions>
+
     </Card>
   );
 }
